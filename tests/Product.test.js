d4250eb31f115c5c271ab5c23c837b6074145a9f
Ronak <79803404+Jrgohil@users.noreply.github.com>
2021-04-08 08:30:28 +0530

    4 more test added

--- tests/Product.test.js
+++ tests/Product.test.js
@@ -26,5 +26,34 @@ test("Add Product Test", async (done) => {
 
 test("Add Product Button Working", async () => {
   const res = await request(app).get("/retail");
+  //If status is 300 or above means redirect on button clicking is working it means button is working
+  expect(res.status).toEqual(302);
+});
+
+test("Update Product Test", async (done) => {
+  const product = {
+    SKU: "134",
+    Keywords: "nothing",
+    Description: "nothing",
+    URL: "http://amazon.com",
+    ProductId: 74,
+  };
+
+  await request(app)
+    .post("/updateProduct")
+    .send(product)
+    .then(async () => {
+      done();
+    })
+    .catch((err) => {
+      // write test for failure here
+      console.log(`Error ${err}`);
+      done();
+    });
+});
+
+test("Update Product Button Working", async () => {
+  const res = await request(app).post("/updateProduct");
+  //If status is 300 or above means redirect on button clicking is working it means button is working
   expect(res.status).toEqual(302);
 });
--- tests/registration.test.js
+++ tests/registration.test.js
@@ -28,3 +28,27 @@ test("Registration Button Working", async () => {
   const res = await request(app).get("/registration");
   expect(res.status).toEqual(200);
 });
+
+test("Login User", async (done) => {
+  const user = {
+    email: "abdc@gmail.com",
+    password: "Abc@123",
+  };
+
+  await request(app)
+    .post("/login")
+    .send(user)
+    .then(async () => {
+      done();
+    })
+    .catch((err) => {
+      // write test for failure here
+      console.log(`Error ${err}`);
+      done();
+    });
+});
+
+test("Login Button Working", async () => {
+  const res = await request(app).get("/login");
+  expect(res.status).toEqual(200);
+});
